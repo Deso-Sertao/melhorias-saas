c07ac4c2b1ec88e603341afde89847feac67a170
Deso-Sertao <35984838+Deso-Sertao@users.noreply.github.com>
2018-02-10 00:05:13 -0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,9 +126,9 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
+          .bindPopup("<b>" + point['PROTOCOLO_ANO'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+          point['ACESSAR_PROTOCOLO']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
