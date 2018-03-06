26ecb5210f232f90ecee26e41545240bf6ac18c6
Deso-Sertao <35984838+Deso-Sertao@users.noreply.github.com>
2018-03-06 12:38:43 -0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Protocolo'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Acessar_gdrive']);
+          point['Acessar_drive']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
